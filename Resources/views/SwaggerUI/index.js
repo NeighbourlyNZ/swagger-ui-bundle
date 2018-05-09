fda996fa9a07a433de6c329579eb365ffb442b34
Oliver Kastler <1730@gmx.net>
2018-05-10 11:37:20 +1200

    updates for webpack + cdn

---
+++ Resources/views/SwaggerUI/index.js
@@ -0,0 +1,7 @@
+import SwaggerUIBundle from '@root/vendor/swagger-api/swagger-ui/dist/swagger-ui-bundle.js';
+import SwaggerUIStandalonePreset from '@root/vendor/swagger-api/swagger-ui/dist/swagger-ui-standalone-preset.js';
+
+window.neighbourly = window.neighbourly || {};
+
+window.neighbourly.SwaggerUIBundle = SwaggerUIBundle;
+window.neighbourly.SwaggerUIStandalonePreset = SwaggerUIStandalonePreset;
